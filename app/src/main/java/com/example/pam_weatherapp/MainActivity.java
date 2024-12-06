1ba6c64f64e4acefd174fcc6f7a414f8155f43d3
tirey93 <tirey93@gmail.com>
2024-12-06 16:19:04 +0100

    added horizontal layout on fragments

--- app/src/main/java/com/example/pam_weatherapp/MainActivity.java
+++ app/src/main/java/com/example/pam_weatherapp/MainActivity.java
@@ -11,6 +11,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.pam_weatherapp.fragments.FragmentBottom;
+import com.example.pam_weatherapp.fragments.FragmentMiddle;
+import com.example.pam_weatherapp.fragments.FragmentTop;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +41,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        FragmentTop fragmentTop = new FragmentTop();
+        FragmentMiddle fragmentMiddle = new FragmentMiddle();
+        FragmentBottom fragmentBottom = new FragmentBottom();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.add(R.id.layoutTop, fragmentTop);
+        ft.add(R.id.layoutMiddle, fragmentMiddle);
+        ft.add(R.id.layoutBottom, fragmentBottom);
+        ft.commit();
     }
 }
---
+++ app/src/main/java/com/example/pam_weatherapp/fragments/FragmentTop.java
@@ -0,0 +1,24 @@
+package com.example.pam_weatherapp.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.pam_weatherapp.R;
+
+
+public class FragmentTop extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_top, container, false);
+
+        return view;
+    }
+}
